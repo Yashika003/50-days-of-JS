d3e864aed5e8d22d6ef97bb1f8b1e9d55ce97bde
Yashika003 <117157340+Yashika003@users.noreply.github.com>
2022-12-29 08:56:54 +0530

    Update Day_1.js

--- Day_1.js
+++ Day_1.js
@@ -1,4 +1,5 @@
-// create a function to generate randum numbers between a given range with maximum and minimum numbers inclusive.
+// create a function to generate randum numbers between a given range with maximum and 
+//minimum numbers inclusive.
 function random_num(min, max) {
   let y = max-min+1
   let x = Math.floor((Math.random() * (y)) +min);
